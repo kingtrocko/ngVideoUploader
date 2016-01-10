2fb2879f064af6517d60a1fe82787911f6a7a738
Nahum Fabian <nahum_fabian13@hotmail.com>
2016-01-10 14:15:36 -0600

    implemented wistia upload API

--- public/js/ng-video-uploader.js
+++ public/js/ng-video-uploader.js
@@ -1,7 +1,9 @@
 
 var uploader = angular.module('ngVideoUploader', []);
 
-uploader.directive('videoUploader', function(){
+uploader.directive('videoUploader',['$http', function($http){
+    var uploadUrl = 'https://upload.wistia.com?api_password=0f2cfe85b5a918b95dfaf24bc72833eabb5d3a4d24d80b5dc8efb47bcb6e0095';
+    
     return {
       restrict : 'E',
       scope: {},
@@ -10,19 +12,52 @@ uploader.directive('videoUploader', function(){
           
           (function initController(){
               $scope.showInput = false;
+              $scope.encodedFile = '';
               
               $('#fileupload').fileupload({
-                  url: '/upload',
-                  autoUpload: true
+                  url: uploadUrl,
+                  autoUpload: true,
+                  multipart: true
               })
+                .on('fileuploadadd', function(e, data){
+                    if(data.files.length > 0){
+                        var file = data.files[0];
+                        encodeTheFile(file);
+                        
+                        console.log($scope.encodedFile);
+                        
+                        var formData = new FormData();
+                        formData.append('file', $scope.encodedFile);
+                        
+                        $(this).fileupload('option', 'formData', formData );
+                        data.submit();
+                    }
+                    debugger;
+                    console.log(data);
+                })
+                .on('fileuploadsubmit', function(e, data){
+                    
+                })
                 .on('fileuploadfail', function(e, data){
                     console.log(e);
                 })
                 .on('fileuploadprogress', function (e, data) {
                     var progress = parseInt(data.loaded / data.total * 100, 10);
                     $('#progress .progress-bar').css('width', progress + '%');
+                })
+                .on('fileuploadalways', function(e, data){
+                    console.log(data);
                 });
               
+              
+              var encodeTheFile = function(file){
+                   var reader = new FileReader();
+                    reader.onload = function(readerEvt) {
+                        var binaryString = readerEvt.target.result;
+                        $scope.encodedFile = btoa(binaryString);
+                    };
+                    reader.readAsBinaryString(file);
+              };
               
               
           })();
@@ -36,4 +71,4 @@ uploader.directive('videoUploader', function(){
           });
       }
     };
-});
+}]);
